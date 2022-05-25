5143e8ed42e233bb99d626e6dd7092a4608b9906
benbaessler <mail@benbaessler.com>
2022-05-25 12:35:26 +0100

    Removed Tooltip from Topbar

--- src/components/ContactTip/index.tsx
+++
@@ -1,9 +0,0 @@
-import Tooltip from 'react-bootstrap/Tooltip'
-
-export default function renderContactTip(props: any) {
-  return (
-    <Tooltip {...props}>
-      You can also message me on Twitter! 🙂
-    </Tooltip>
-  )
-}
