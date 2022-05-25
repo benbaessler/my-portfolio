c30e41f9e858c8e6fa67ab0d873f85485dbedef3
benbaessler <mail@benbaessler.com>
2022-05-25 12:55:05 +0100

    Added Soon page

--- src/sections/Work/index.tsx
+++ src/sections/Work/index.tsx
@@ -4,6 +4,9 @@ export default function Work() {
   return <div className="section">
     <div className="workContainer">
       <h2>My Work</h2>
+      <div id="lendfiProjectContainer">
+        
+      </div>
     </div>
   </div>
 }
