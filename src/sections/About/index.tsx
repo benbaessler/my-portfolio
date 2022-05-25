88bd69e9fd86b3a65e9310876d6f18cd206046cb
benbaessler <mail@benbaessler.com>
2022-05-25 11:33:13 +0100

    Built About section

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -4,6 +4,12 @@ export default function About() {
   return <div className="section">
     <div className="aboutContainer">
       <h2>About Me</h2>
+      <p>
+        I am 18 years old and currently based in London, UK.
+        Self-taught developer since 2019 and specialized in blockchain development since 2021.
+        My goal is to leave an impact in the Web 3 space by building solutions that will help 
+        blockchain technology reach global adoption.
+      </p>
     </div>
   </div>
 }
