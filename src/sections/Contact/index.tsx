9f2907bc7814015e8a57daf39f3d66334a8d5bd3
benbaessler <mail@benbaessler.com>
2022-06-17 20:43:10 +0200

    Added Contact section

--- src/sections/Contact/index.tsx
+++ src/sections/Contact/index.tsx
@@ -1,7 +1,13 @@
 import './style.css'
 
 export default function Contact() {
-  return <div className="contactSection">
-       
+  return <div className="section contactSection">
+    <h2>Contact Me</h2>
+    <p>
+      I am actively looking for work opportunities, so feel free to get in touch
+      if you are in need of a developer, or even if you just want to say hello. I will 
+      try to get back to you as soon as I can!
+    </p>
+    <div className="button">Get in touch</div>
   </div>
 }
