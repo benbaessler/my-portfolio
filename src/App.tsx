49fa15c3d7e67caf87f9f60340d2997de51f29ea
benbaessler <mail@benbaessler.com>
2022-06-17 21:02:07 +0200

    Files updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import Topbar from './components/Topbar';
 import Home from './sections/Home'
 import About from './sections/About'
 import Work from './sections/Work'
-import Soon from './sections/Soon'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer';
 
 function App() {
 
-  return <div className="main">
-    <Topbar/>
-    <Home/>
-    <About/>
-    <Work/>
-    <Soon/>
+  return <div style={{ backgroundColor: "#292b47"}}>
+    <div className="main">
+      <Topbar/>
+      <Home/>
+      <About/>
+      <Work/>
+      <Contact/>
+    </div>  
+    <Footer/>
   </div>
+    
 
 }
 
