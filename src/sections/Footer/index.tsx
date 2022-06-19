2c5c07485638a75800d91f8260b60c308d1fdd7e
benbaessler <mail@benbaessler.com>
2022-06-19 14:30:37 +0200

    Added logo credit

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -3,5 +3,6 @@ import './style.css'
 export default function Footer() {
   return <div className="footer">
     <span>© 2022 Ben Baessler. All rights reserved.</span>
+    <span>Logo by <a href="https://www.vecteezy.com/" target="_blank">vecteezy.com</a></span>
   </div>
 }
