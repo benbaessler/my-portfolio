d8f83688415f12eac197aca21290cb62d0b7276a
NisooJadhav <nisoojadhav@gmail.com>
2022-06-20 16:16:09 +0530

    added id that will user to contact area

--- src/sections/Contact/index.tsx
+++ src/sections/Contact/index.tsx
@@ -1,13 +1,17 @@
-import './style.css'
+import "./style.css";
 
 export default function Contact() {
-  return <div className="section contactSection">
-    <h2>Contact Me</h2>
-    <p>
-      I am actively looking for work opportunities, so feel free to get in touch
-      if you are in need of a developer, or even if you just want to say hello. I will 
-      try to get back to you as soon as I can!
-    </p>
-    <a href="mailto:hello@benbaessler.com"><div className="button">Get in touch</div></a>
-  </div>
+  return (
+    <div className="section contactSection">
+      <h2 id="contact">Contact Me</h2>
+      <p>
+        I am actively looking for work opportunities, so feel free to get in
+        touch if you are in need of a developer, or even if you just want to say
+        hello. I will try to get back to you as soon as I can!
+      </p>
+      <a href="mailto:hello@benbaessler.com">
+        <div className="button">Get in touch</div>
+      </a>
+    </div>
+  );
 }
