4f8407f422d12d5cb3dc1c0867e11bae1ecf242d
benbaessler <mail@benbaessler.com>
2022-06-21 12:29:19 +0200

    Added scrollTo nav links

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useRef } from 'react'
 
 import Topbar from './components/Topbar';
 import Home from './sections/Home'
@@ -7,15 +8,59 @@ import Work from './sections/Work'
 import Contact from './sections/Contact'
 import Footer from './components/Footer';
 
+import Logo from './assets/logo.png'
+import twitterIcon from './assets/icons/media/twitter.png'
+import linkedInIcon from './assets/icons/media/linkedIn.png'
+import gitHubIcon from './assets/icons/media/github.png'
+
 function App() {
+
+  const homeRef = useRef(null)
+  const aboutRef = useRef(null)
+  const workRef = useRef(null)
+  const contactRef = useRef(null)
+
+  const scrollTo = (ref: React.MutableRefObject<any>) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' })
+  }
 
   return <div style={{ backgroundColor: "#292b47"}}>
     <div className="main">
-      <Topbar/>
-      <Home/>
-      <About/>
-      <Work/>
-      <Contact/>
+      <div className="topBar">
+        <div className="logoContainer" onClick={() => scrollTo(homeRef)}>
+          <img src={Logo}/>
+        </div>
+        <div className="navbarLinkContainer">
+          <div className="navbarLinks">
+            <a onClick={() => scrollTo(aboutRef)}>About</a>
+            <a onClick={() => scrollTo(workRef)}>Work</a>
+            <a onClick={() => scrollTo(contactRef)}>Contact</a>
+          </div>
+          <div style={{ marginLeft: '2rem' }}>
+            <a href="https://twitter.com/ohCurles" target="_blank" rel="noopener noreferrer">
+              <img src={twitterIcon}/>
+            </a>
+            <a href="https://github.com/benbaessler" target="_blank" rel="noopener noreferrer">
+              <img src={gitHubIcon} style={{ height: '35px' }}/>
+            </a>
+            <a href="https://linkedin.com/in/benbaessler/" target="_blank" rel="noopener noreferrer">
+              <img src={linkedInIcon} style={{ height: '32px' }}/>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div ref={homeRef}>
+        <Home/>
+      </div>
+      <div ref={aboutRef}>
+        <About/>
+      </div>
+      <div ref={workRef}>
+        <Work/>
+      </div>
+      <div ref={contactRef}>
+        <Contact/>
+      </div>
     </div>  
     <Footer/>
   </div>
