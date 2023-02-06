cea9f3a8818bd0fe226b2ad7623d664c2e877cf3
benbaessler <mail@benbaessler.com>
2023-02-06 15:10:22 +0000

    update: contact me

--- src/sections/Contact/index.tsx
+++ src/sections/Contact/index.tsx
@@ -5,11 +5,10 @@ export default function Contact() {
     <div className="section contactSection">
       <h2>Contact Me</h2>
       <p>
-        I am not actively looking for work opportunities at this time, but I am
-        always happy to connect and discuss potential projects or
-        collaborations. If you are interested in working together or just want
-        to say hello, please don't hesitate to get in touch. I will do my best
-        to get back to you as soon as I can.
+        I am open to work opportunities and eager to connect and discuss
+        potential projects or collaborations. If you are interested in working
+        together or just want to say hello, please feel free to reach out. I
+        will do my best to get back to you as soon as I can.
       </p>
       <a href="mailto:hello@benbaessler.com">
         <div className="button">Get in touch</div>
