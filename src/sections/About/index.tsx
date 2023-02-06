156bf648a8ae293433f5818ecffc084615145a85
benbaessler <mail@benbaessler.com>
2023-02-06 13:17:54 +0000

    feat: update about me

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -10,33 +10,29 @@ export default function About() {
         <div className="aboutContentContainer">
           <div id="aboutContentSection">
             <p>
-              I am a German-American 18-year-old developer based in London, UK.
-              My journey as a software developer started back in 2019 when I
-              took an online <span>Python</span> course. Since then, coding has
-              become my passion.
-            </p>
-            <p>
-              During COVID-19, I spent my time exploring different development
-              'niches' to find something that I wanted to specialize in. I
-              started teaching myself various programming languages including{" "}
+              I am a London-based, German-American software developer with a
+              passion for coding and blockchain technology. My journey began in
+              2019 when I took an online <span>Python</span> course, which
+              sparked my passion for coding. Since then, I have continued to
+              develop my skills in various programming languages such as{" "}
               <span>Flutter (Dart)</span>, <span>HTML & CSS</span>,{" "}
-              <span>Swift</span> and <span>JavaScript</span>.
+              <span>Swift</span>, and
+              <span> JavaScript</span>. My diverse background in these
+              technologies has allowed me to bring a unique perspective to my
+              projects.
             </p>
             <p>
-              When I learned about blockchain in early 2021, I was immediately
-              intrigued by the potential of this technology. It was clear to me,
-              this is what I wanted to work on. Since then, I've actively
-              participated in the community, while learning about the{" "}
-              <span>crypto</span> & <span>web3</span> space.
+              In early 2021, I discovered the potential of blockchain technology
+              and immediately knew that this was the area I wanted to focus on.
+              I have actively participated in the community, continuously
+              learning about the crypto and web3 space. After teaching myself
+              <span> Solidity</span> and <span>React</span>, I landed my first
+              job as a part-time blockchain developer for a web 3 start-up in
+              August 2021.
             </p>
             <p>
-              In August 2021, after teaching myself <span>Solidity</span> and{" "}
-              <span>React</span> to build <span>dApps</span> /{" "}
-              <span>smart contracts</span>, I landed my first job as a part-time
-              blockchain developer for a web 3 start-up.
-            </p>
-            <p>
-              I have joined the team at{" "}
+              From October 2022 to February 2023, I had the opportunity to work
+              at{" "}
               <a
                 href="https://morpho.xyz"
                 target="_blank"
@@ -44,11 +40,18 @@ export default function About() {
               >
                 Morpho Labs
               </a>{" "}
-              as a Software Engineer in January 2023. In this role, I have the
-              opportunity to work on building software for a decentralized lending
-              platform and to continue learning and growing as a developer. I am
-              looking forward to tackling the challenges and opportunities that
-              come with working on exciting projects at Morpho.
+              as a Software Engineer. During this time, I gained valuable
+              experience building software for a decentralized lending platform
+              and continued to grow as a developer. I am now open to new
+              opportunities and eager to bring my skills and passion to another
+              exciting project.
+            </p>
+            <p>
+              Beyond my career as a software developer, I have entrepreneurial
+              ambitions and am always seeking new challenges and opportunities
+              to innovate and create in the web3 space. I am excited about the
+              future of decentralized technology and eager to be a part of
+              shaping the next generation of the web.
             </p>
           </div>
 
@@ -56,7 +59,7 @@ export default function About() {
             <div className="aboutImageContainer">
               <img src={MyImage} alt="Ben Baessler portrait" />
             </div>
-            <div>
+            {/* <div>
               <p>My technical skills include:</p>
               <ul className="skillsList">
                 <li>Solidity</li>
@@ -66,7 +69,7 @@ export default function About() {
                 <li>Python</li>
                 <li>HTML & CSS</li>
               </ul>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
