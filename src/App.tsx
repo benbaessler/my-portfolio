078212085a5eac26f66f839feadecc2aa2476c59
benbaessler <mail@benbaessler.com>
2023-02-06 14:08:16 +0000

    feat: add more socials

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,97 @@
-import './App.css';
-import { useRef } from 'react'
+import "./App.css";
+import { useRef } from "react";
 
-import Topbar from './components/Topbar';
-import Home from './sections/Home'
-import About from './sections/About'
-import Work from './sections/Work'
-import Contact from './sections/Contact'
-import Footer from './components/Footer';
+import Home from "./sections/Home";
+import About from "./sections/About";
+import Work from "./sections/Work";
+import Contact from "./sections/Contact";
+import Footer from "./components/Footer";
 
-import Logo from './assets/logo.png'
-import twitterIcon from './assets/icons/media/twitter.png'
-import linkedInIcon from './assets/icons/media/linkedIn.png'
-import gitHubIcon from './assets/icons/media/github.png'
+import Logo from "./assets/logo.png";
+import twitterIcon from "./assets/icons/media/twitter.png";
+import linkedInIcon from "./assets/icons/media/linkedIn.png";
+import gitHubIcon from "./assets/icons/media/github.png";
+import telegramIcon from "./assets/icons/media/telegram.png";
+import etherscanIcon from "./assets/icons/media/etherscan.png";
 
 function App() {
-
-  const homeRef = useRef(null)
-  const aboutRef = useRef(null)
-  const workRef = useRef(null)
-  const contactRef = useRef(null)
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const workRef = useRef(null);
+  const contactRef = useRef(null);
 
   const scrollTo = (ref: React.MutableRefObject<any>) => {
-    ref.current.scrollIntoView({ behavior: 'smooth' })
-  }
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-  return <div style={{ backgroundColor: "#292b47"}}>
-    <div className="main">
-      <div className="topBar">
-        <div className="logoContainer" onClick={() => scrollTo(homeRef)}>
-          <img src={Logo}/>
-        </div>
-        <div className="navbarLinkContainer">
-          <div className="navbarLinks">
-            <a onClick={() => scrollTo(aboutRef)}>About</a>
-            <a onClick={() => scrollTo(workRef)}>Work</a>
-            <a onClick={() => scrollTo(contactRef)}>Contact</a>
+  return (
+    <div style={{ backgroundColor: "#292b47" }}>
+      <div className="main">
+        <div className="topBar">
+          <div className="logoContainer" onClick={() => scrollTo(homeRef)}>
+            <img src={Logo} />
           </div>
-          <div style={{ marginLeft: '2rem' }}>
-            <a href="https://twitter.com/mrbaessler" target="_blank" rel="noopener noreferrer">
-              <img src={twitterIcon}/>
-            </a>
-            <a href="https://github.com/benbaessler" target="_blank" rel="noopener noreferrer">
-              <img src={gitHubIcon} style={{ height: '35px' }}/>
-            </a>
-            <a href="https://linkedin.com/in/benbaessler/" target="_blank" rel="noopener noreferrer">
-              <img src={linkedInIcon} style={{ height: '32px' }}/>
-            </a>
+          <div className="navbarLinkContainer">
+            <div className="navbarLinks">
+              <a onClick={() => scrollTo(aboutRef)}>About</a>
+              <a onClick={() => scrollTo(workRef)}>Work</a>
+              <a onClick={() => scrollTo(contactRef)}>Contact</a>
+            </div>
+            <div className="socialLinkContainer">
+              <a
+                href="https://twitter.com/mrbaessler"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={twitterIcon} />
+              </a>
+              <a
+                href="https://github.com/benbaessler"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={gitHubIcon}/>
+              </a>
+              <a
+                href="https://linkedin.com/in/benbaessler/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={linkedInIcon} />
+              </a>
+              <a
+                href="https://t.me/benbaessler"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={telegramIcon} />
+              </a>
+              <a
+                href="https://etherscan.io/address/curles.eth"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={etherscanIcon} />
+              </a>
+            </div>
           </div>
         </div>
+        <div ref={homeRef}>
+          <Home />
+        </div>
+        <div ref={aboutRef}>
+          <About />
+        </div>
+        <div ref={workRef}>
+          <Work />
+        </div>
+        <div ref={contactRef}>
+          <Contact />
+        </div>
       </div>
-      <div ref={homeRef}>
-        <Home/>
-      </div>
-      <div ref={aboutRef}>
-        <About/>
-      </div>
-      <div ref={workRef}>
-        <Work/>
-      </div>
-      <div ref={contactRef}>
-        <Contact/>
-      </div>
-    </div>  
-    <Footer/>
-  </div>
-    
-
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
