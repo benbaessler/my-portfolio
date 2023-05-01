dc0dd1e7b9faafe776fc91938f077594b32afb96
benbaessler <mail@benbaessler.com>
2023-05-01 22:54:22 +0100

    chore: add web analytics

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import HttpsRedirect from 'react-https-redirect';
+import { inject } from "@vercel/analytics";
 
 import App from './App';
 
-
+inject();
 ReactDOM.render(
   <React.StrictMode>
     <HttpsRedirect>
