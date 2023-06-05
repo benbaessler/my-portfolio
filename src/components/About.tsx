d8b8b8d5d135d771ce14ffa9460d931b65b6cb9a
benbaessler <mail@benbaessler.com>
2023-06-05 23:50:28 +0100

    feat: about me content

---
+++ src/components/About.tsx
@@ -0,0 +1,31 @@
+const About = () => (
+  <div className="page-container">
+    <h2>About Me</h2>
+    <div className="flex flex-wrap">
+      <div>
+        <p>
+          As a German-American software developer based in London, I’ve been
+          honing my skills in coding and blockchain since 2019. I’ve developed
+          proficiency in languages like Python, Flutter, HTML & CSS, Swift, and
+          JavaScript, contributing a unique perspective to projects.
+        </p><br />
+        <p>
+          In 2021, I shifted focus to blockchain, educating myself about crypto
+          and web3, and learning Solidity and React. By August, I joined a web 3
+          start-up as a part-time blockchain developer. From October 2022 to
+          February 2023, I was a Software Engineer at Morpho Labs, gaining
+          experience building decentralized lending platform software.
+        </p><br />
+        <p>
+          Now, I’m open to new opportunities where I can apply my passion and
+          skills. Aside from software development, I’m driven by
+          entrepreneurship, always seeking new challenges and innovations in the
+          web3 space, with a keen interest in shaping the future of
+          decentralized technology.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default About;
