ab64fe3109fb5aa2ca19a38ffc7e0d4fd037fd97
benbaessler <mail@benbaessler.com>
2023-06-07 00:11:09 +0100

    chore: fix about styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,43 @@
 import Image from "next/image";
 import Portait from "@/assets/me.png";
 
-
 const About = () => (
-  <div className="page-container">
-    <h2>About Me</h2>
-    <div className="flex flex-wrap">
-      <div className="md:w-1/2">
-        <p>
-          As a German-American software developer based in London, I’ve been
-          honing my skills in coding and blockchain since 2019. I’ve developed
-          proficiency in languages like Python, Flutter, HTML & CSS, Swift, and
-          JavaScript, contributing a unique perspective to projects.
-        </p>
-        <br />
-        <p>
-          In 2021, I shifted focus to blockchain, educating myself about crypto
-          and web3, and learning Solidity and React. By August, I joined a web 3
-          start-up as a part-time blockchain developer. From October 2022 to
-          February 2023, I was a Software Engineer at Morpho Labs, gaining
-          experience building decentralized lending platform software.
-        </p>
-        <br />
-        <p>
-          Now, I’m open to new opportunities where I can apply my passion and
-          skills. Aside from software development, I’m driven by
-          entrepreneurship, always seeking new challenges and innovations in the
-          web3 space, with a keen interest in shaping the future of
-          decentralized technology.
-        </p>
+  <div className="page-container flex align-center justify-center">
+    <div className="flex flex-wrap justify-center items-center mt-6 max-w-screen-2xl">
+      <div className="flex flex-col py-5 lg:w-1/2 items-center text-center md:items-start md:text-start">
+        <h2>About Me</h2>
+        <div className="text-lg">
+          <p>
+            As a German-American software developer based in London, I’ve been
+            honing my skills in coding and blockchain since 2019. I’ve developed
+            proficiency in languages like Python, Flutter, HTML & CSS, Swift,
+            and JavaScript, contributing a unique perspective to projects.
+          </p>
+          <br />
+          <p>
+            In 2021, I shifted focus to blockchain, educating myself about
+            crypto and web3, and learning Solidity and React. By August, I
+            joined a web 3 start-up as a part-time blockchain developer.
+          </p>
+          <br />
+          <p>
+            From October 2022 to February 2023, I was a Software Engineer at
+            Morpho Labs, gaining experience building decentralized lending
+            platform software. Now, I’m open to new opportunities where I can
+            apply my passion and skills.
+          </p>
+          <br />
+          <p>
+            Aside from software development, I’m driven by entrepreneurship,
+            always seeking new challenges and innovations in the web3 space,
+            with a keen interest in shaping the future of decentralized
+            technology.
+          </p>
+        </div>
       </div>
+      {/* TODO: increase top margin */}
       <div className="flex flex-col md:w-1/2 justify-center items-center">
-        <div className="w-1/2 max-w-lg min-w-sm">
+        <div className="w-1/2 min-w-sm">
           <Image
             src={Portait}
             alt="Ben Baessler"
