6bbb3c8ac50e0158f87cb525064a423af0a7a166
benbaessler <mail@benbaessler.com>
2023-06-07 00:51:24 +0100

    chore: integrate scroll button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,37 @@
+import { useRef } from "react";
 import { Inter } from "next/font/google";
 
 import Navbar from "@/components/Navbar";
+import ScrollButton from "@/components/ScrollButton";
 import LandingPage from "@/components/Landing";
 import About from "@/components/About";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const landingRef = useRef<HTMLDivElement | null>(null);
+  const aboutRef = useRef<HTMLDivElement | null>(null);
+
+  const handleScroll = () => {
+    if (aboutRef.current && window.scrollY < aboutRef.current.offsetTop) {
+      aboutRef.current.scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
   return (
     <main>
       <div className="hidden md:inline">
         <Navbar />
       </div>
-      <LandingPage />
-      <About />
+      <div ref={landingRef}>
+        <LandingPage />
+      </div>
+      <div ref={aboutRef}>
+        <About />
+      </div>
+      <ScrollButton handleScroll={handleScroll} />
     </main>
   );
 }
