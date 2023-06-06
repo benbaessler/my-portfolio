d1e2708268ddaf6db11809dba1c5e9564d81710f
benbaessler <mail@benbaessler.com>
2023-06-07 00:51:07 +0100

    refactor: button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,14 @@
-export interface ButtonProps {
+interface Props {
   text: string;
   href: string;
 }
 
-const Button = ({ text, href }: ButtonProps) => (
+const Button = ({ text, href }: Props) => (
   <a
     href={href}
     target="_blank"
     rel="noopener noreferrer"
-    className="inline-block bg-indigo-700 px-8 py-4 rounded-full transition-colors duration-200 hover:bg-indigo-800"
+    className="button inline-block px-8 py-4"
   >
     {text}
   </a>
