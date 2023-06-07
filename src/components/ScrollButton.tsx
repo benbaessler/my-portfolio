af1a60a70a56426f49bb5764085d0172d9320074
benbaessler <mail@benbaessler.com>
2023-06-07 03:14:11 +0100

    chore: update scroll button styling

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -6,7 +6,7 @@ interface Props {
 const ScrollButton = ({ handleScroll }: Props) => (
   <div
     onClick={handleScroll}
-    className="button fixed w-11 h-11 bottom-10 md:inset-x-0 md:mx-auto right-10 md:right-0 transform hover:scale-105 cursor-pointer flex items-center justify-center"
+    className="button fixed w-11 h-11 bottom-7 md:inset-x-0 md:mx-auto right-7 md:right-0 transform hover:scale-105 cursor-pointer flex items-center justify-center"
   >
     <ArrowDownwardIcon fontSize="medium" style={{ color: "white" }} />
   </div>
