8dafda2bca1620a3f323556f064e97c88b3b1802
benbaessler <mail@benbaessler.com>
2023-06-07 03:55:02 +0100

    refactor: navbar social links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,12 +6,41 @@ import LensIcon from "@/assets/icons/lens.png";
 import LinkedInIcon from "@/assets/icons/linkedin.png";
 import Button from "@/components/Button";
 
+const SOCIAL_ICONS = [
+  {
+    name: "Twitter",
+    image: TwitterIcon,
+    link: "https://twitter.com/mrbaessler",
+  },
+  {
+    name: "Lens profile",
+    image: LensIcon,
+    link: "https://lensfrens.xyz/benbaessler.lens",
+  },
+  {
+    name: "LinkedIn",
+    image: LinkedInIcon,
+    link: "https://linkedin.com/in/benbaessler",
+  },
+];
+
 const Navbar: FC = () => {
   return (
     <nav className="fixed top-0 right-0 py-6 px-6 space-x-5 text-xl flex items-center">
-      <Image src={TwitterIcon} alt="Twitter" className="h-11 w-11 cursor-pointer opacity-90	hover:opacity-100"/>
-      <Image src={LensIcon} alt="Lens profile" className="h-11 w-11 cursor-pointer opacity-90	hover:opacity-100"/>
-      <Image src={LinkedInIcon} alt="LinkedIn" className="h-11 w-11 cursor-pointer opacity-90	hover:opacity-100"/>
+      {SOCIAL_ICONS.map((icon) => (
+        <a
+          key={icon.name}
+          href={icon.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src={icon.image}
+            alt={icon.name}
+            className="h-11 w-11 cursor-pointer opacity-90	hover:opacity-100"
+          />
+        </a>
+      ))}
       <Button text="View my GitHub" href="https://github.com/benbaessler" />
     </nav>
   );
