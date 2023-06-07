f1ea9db08c62a73c8620bc47179df466c09a8819
benbaessler <mail@benbaessler.com>
2023-06-07 03:12:37 +0100

    feat: navbar social links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import type { FC } from "react";
-import Link from "next/link";
+import Image from "next/image";
 
+import TwitterIcon from "@/assets/icons/twitter.png";
+import LensIcon from "@/assets/icons/lens.png";
+import LinkedInIcon from "@/assets/icons/linkedin.png";
 import Button from "@/components/Button";
 
 const Navbar: FC = () => {
   return (
-    <nav className="fixed top-0 right-0 py-7 px-10 space-x-10 text-xl">
+    <nav className="fixed top-0 right-0 py-6 px-6 space-x-5 text-xl flex items-center">
+      <Image src={TwitterIcon} alt="Twitter" className="h-11 w-11 cursor-pointer opacity-90	hover:opacity-100"/>
+      <Image src={LensIcon} alt="Lens profile" className="h-11 w-11 cursor-pointer opacity-90	hover:opacity-100"/>
+      <Image src={LinkedInIcon} alt="LinkedIn" className="h-11 w-11 cursor-pointer opacity-90	hover:opacity-100"/>
       <Button text="View my GitHub" href="https://github.com/benbaessler" />
     </nav>
   );
