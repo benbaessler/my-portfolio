97f84ac560c26c201a5713945bdd8f9d87d8446c
benbaessler <mail@benbaessler.com>
2023-06-07 01:04:11 +0100

    feat: scroll button animation

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -6,9 +6,9 @@ interface Props {
 const ScrollButton = ({ handleScroll }: Props) => (
   <div
     onClick={handleScroll}
-    className="button fixed bottom-10 md:inset-x-0 md:mx-auto right-10 md:right-0 h-12 w-12 cursor-pointer flex items-center justify-center text-lg"
+    className="button fixed w-11 h-11 bottom-10 md:inset-x-0 md:mx-auto right-10 md:right-0 transform hover:scale-105 cursor-pointer flex items-center justify-center"
   >
-    <ArrowDownwardIcon fontSize="large" />
+    <ArrowDownwardIcon fontSize="medium" style={{ color: "white" }} />
   </div>
 );
 
