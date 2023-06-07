184a185fc01d205693065646af945503768025dc
benbaessler <mail@benbaessler.com>
2023-06-07 02:27:10 +0100

    chore: remove navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,18 +6,6 @@ import Button from "@/components/Button";
 const Navbar: FC = () => {
   return (
     <nav className="fixed top-0 right-0 py-7 px-10 space-x-10 text-xl">
-      {[
-        ["Home", "#home"],
-        ["About", "#about"],
-        ["Work", "#work"],
-        ["Contact", "#contact"],
-      ].map(([title, href]) => (
-        <Link href={href} key={title}>
-          <span className="cursor-pointer hover:text-sky-400 transition-colors duration-200">
-            {title}
-          </span>
-        </Link>
-      ))}
       <Button text="View my GitHub" href="https://github.com/benbaessler" />
     </nav>
   );
