6e5db6c7404500c052ec96bca63db712c33a51df
benbaessler <mail@benbaessler.com>
2023-09-23 04:23:23 +0100

    feat: project links

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,36 +1,54 @@
 import Project from "./Project";
 import Job from "./Job";
 
+import Button from "./Button";
+
 const ProjectsPage = () => {
   return (
     <div className="page-container flex align-center justify-center">
       <div className="flex flex-wrap justify-between w-full max-w-screen-xl">
-        <div className="flex flex-col lg:w-1/2">
+        <div className="flex flex-col lg:w-1/2 pr-10">
           <h2>My Projects</h2>
           <div>
             <Project
               title="Lens Echo"
-              link="https://twitter.com/mrbaessler/status/1658887825297637387"
               description="The first Web3 social interface on Discord, built with Lens Protocol. Lens Echo is bridging the gap between Web3 social and Discord, the most widely adopted communication app for online communities."
               tags={["TypeScript", "Express", "Next.js"]}
-              githubLink="https://github.com/benbaessler/lens-echo"
+              links={[
+                {
+                  title: "Try it out",
+                  href: "https://lensecho.xyz",
+                },
+                {
+                  title: "GitHub",
+                  href: "https://github.com/benbaessler/lens-echo",
+                },
+                {
+                  title: "Lens",
+                  href: "https://share.lens.xyz/u/lensecho.lens",
+                },
+                {
+                  title: "Twitter",
+                  href: "https://twitter.com/lensechoxyz",
+                },
+              ]}
             />
             <Project
               title="LendFi"
               description="A platform for lending and borrowing ETH for NFT collateral (ERC-721)."
               tags={["Solidity", "TypeScript", "React"]}
-              githubLink="https://github.com/benbaessler/lendfi-interface"
+              links={[
+                {
+                  title: "GitHub",
+                  href: "https://github.com/benbaessler/lendfi-interface",
+                },
+              ]}
             />
-            <p className="text-slate-200">
-              <a
+            <p className="text-slate-200 flex pt-2 pb-12">
+              <Button
+                text="View all projects on GitHub"
                 href="https://github.com/benbaessler"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-white"
-              >
-                You can view all of my projects on GitHub
-              </a>
-              .
+              />
             </p>
           </div>
         </div>
