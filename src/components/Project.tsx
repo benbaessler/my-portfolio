180905e1d1be55fe167af20c40bcb26596faca6f
benbaessler <mail@benbaessler.com>
2023-09-23 04:22:40 +0100

    feat: project links

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,41 +1,43 @@
-import Image, { StaticImageData } from "next/image";
-import GitHubIcon from "@/assets/icons/github.png";
-import CallMadeIcon from "@mui/icons-material/CallMade";
-
 interface Props {
   title: string;
   description: string;
   tags: string[];
-  githubLink: string;
-  link?: string;
+  links: Link[];
 }
 
-const Project = ({ title, description, tags, githubLink, link }: Props) => (
+interface Link {
+  title: string;
+  href: string;
+}
+
+const Project = ({ title, description, tags, links }: Props) => (
   <div className="bg-slate flex flex-col sm:flex-row mb-10 max-w-xl">
     <div className="flex flex-col space-y-4">
       <div className="flex items-center">
-        <a href={githubLink} target="_blank" rel="noopener noreferrer">
-          <h3 className="text-xl font-semibold mr-3 hover:text-slate-200">
-            {title}
-          </h3>
-        </a>
-        {link && (
-          <a
-            href={link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="opacity-80 hover:opacity-100"
-          >
-            <CallMadeIcon />
-          </a>
-        )}
+        <h3 className="text-xl font-semibold mr-3 hover:text-slate-200">
+          {title}
+        </h3>
       </div>
       <p className="text-md">{description}</p>
+
+      <div className="links flex gap-4 text-sm pb-2">
+        {links.map((link) => (
+          <a
+            key={link.title}
+            href={link.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>{link.title}</span>
+          </a>
+        ))}
+      </div>
+
       <div>
         {tags.map((tag) => (
           <span
             key={tag}
-            className="text-xs text-sky-400 bg-indigo-800 rounded-full px-3 py-2 mr-3"
+            className="text-xs text-slate-300	bg-indigo-800 rounded-full px-3 py-2 mr-3"
           >
             {tag}
           </span>
