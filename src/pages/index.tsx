81ea8ca4e5911c0284568f1a1f97ea70474e9295
Ben Baessler <mail@benbaessler.com>
2024-04-15 22:54:22 +0100

    feat: vercel speed insights

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,13 @@ import LandingPage from "@/components/Landing";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => (
   <main>
+    <SpeedInsights />
     <Analytics mode="production" />
     <Navbar />
     <LandingPage />
